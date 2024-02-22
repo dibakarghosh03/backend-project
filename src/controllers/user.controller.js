bcd53ad0a22119f86c08fa9aab44fac6c636ea8b
Dibakar81 <ghoshdibakar81@gmail.com>
2024-02-22 19:42:58 +0530

    Refactor getCurrentUser format

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -232,7 +232,9 @@ const changeCurrentPassword = asyncHandler(async(req,res) => {
 const getCurrentUser = asyncHandler(async (req,res) => {
     return res
             .status(200)
-            .json(200, req.user);
+            .json(
+                new ApiResponse(200,req.user,"User fetched successfully")
+            );
 });
 
 
@@ -342,5 +344,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateCoverImage
+    updateCoverImage,
+    getUserChannelProfile
 };
