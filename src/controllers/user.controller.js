5cd6b1f87fb61f66551e9cc09b462b489f3cbb22
Dibakar81 <ghoshdibakar81@gmail.com>
2024-02-22 20:31:26 +0530

    fix logout functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -137,10 +137,11 @@ const loginUser = asyncHandler(async (req,res) => {
 
 
 const logoutUser = asyncHandler(async(req,res) => {
-    await User.findByIdAndUpdate(req.user._id,
+    await User.findByIdAndUpdate(
+        req.user._id,
         {
-            $set: {
-                refreshToken : undefined
+            $unset: {
+                refreshToken: 1
             }
         },
         {
